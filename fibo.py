50a6baa0f0e961bc9fc216a78786e7a0802bc9ff
Artvisai <artvisab@gmail.com>
2021-12-04 22:53:21 +0300

    fibo correction

--- fibo.py
+++ fibo.py
@@ -1,15 +1,15 @@
 def fib(num):
-    if num == 1:
+    if num == 0:
         return 0
-    if num == 2:
+    if num == 1:
         return 1
     return fib(num - 1) + fib(num - 2)
 
 
 try:
     n = int(input('Input number of position: '))
-    if n < 1:
+    if n < 0:
         raise ValueError
     print('{}-th Fibonacci number is {}'.format(n, fib(n)))
 except ValueError:
-    print('It must be a positive integer!')
+    print('It must be a not negative integer!')
